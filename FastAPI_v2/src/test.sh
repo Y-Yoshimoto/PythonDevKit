11f79ebff86b4c20467278cc6a1882a86cb1cfd8
Y-Yoshimoto <mewyoshimoto@gmail.com>
2025-02-24 15:32:59 +0900

    add: デバッグ機能の強化

--- FastAPI_v2/src/test.sh
+++ FastAPI_v2/src/test.sh
@@ -8,6 +8,9 @@ domein="http://$host:$port"
 # curl ${domein}/methodsample/2
 
 # curl ${domein}/methodsample/2 -X POST -H "Content-Type: application/json" -d '{"id":2, "name":"test", "date": "2024-08-09"}'
-curl ${domein}/methodsample/ -X POST -H "Content-Type: application/json" -d '{"id":2, "name":"test"}'
+# curl ${domein}/methodsample/ -X POST -H "Content-Type: application/json" -d '{"id":2, "name":"test"}'
+
+curl ${domein}/methodsample/ -X PUT -H "Content-Type: application/json" -d '{"id":2, "name":"test"}'
+
 # doc
 # curl ${domein}/docs
